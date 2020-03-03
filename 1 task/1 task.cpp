4af53e3c33598a15b52b8ddf8c89e08e6fabc609
Elizaveta2204 <bashckovaliza@yandex.ru>
2020-03-03 17:32:13 +0300

    relese v1

---
+++ 1 task/1 task.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Queue.h"
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Queue<int> que = Queue<int>(3);
+    que.push(2);
+    que.push(4);
+    que.push(5);
+    Queue<int> que2 = Queue<int>(2);
+    que2.push(19);
+    que2.push(22);
+    Queue<int> mergerQues = merger_Queues(que, que2);
+    for (int i = 0; i < 5; i++)
+        cout << mergerQues.pop() << endl;
+    return 0;
+}
---
+++ 2 task/2 task.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include "BinaryThree.cpp"
+using namespace std;
+int main()
+{
+	BinaryThree<int> th(3);
+	th.Push(2);
+	th.Push(4);
+	th.Push(8);
+	th.Push(1);
+	th.Push(6);
+	th.Push(5);
+	th.Print();
+	th.Printl();
+	th.Pop(3);
+	th.Print();
+}
